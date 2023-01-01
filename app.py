35c8e79ecfe3e52e811e38ef97d332bcd72c6edc
urevoleg <urevolegg@gmail.com>
2023-01-01 18:56:26 +0000

    first version

--- app.py
+++ app.py
@@ -4,13 +4,31 @@ import datetime as dt
 from flask import Flask, jsonify
 
 
+from w1thermsensor import W1ThermSensor
+from libs.pressure import PressureSensor
+
+
+sensor = W1ThermSensor()
+
+def get_temperature():
+    return sensor.get_temperature() 
+
+
+def get_pressure():
+    return PressureSensor(0x77).get_pressure()
+
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
     return jsonify({
-	"dt": dt.datetime.now()
+	"dt": dt.datetime.now(),
+	"sensors": {
+		"t": [{"value": get_temperature(), "type": "ds18b20", "place": "out"}],
+		"p": [{"value": get_pressure(), "type": "bmp180"}]
+	}
 })
 
 
