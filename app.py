7cdab0db3f3e398b5ba05598e66439e213682ccc
urevoleg <urevolegg@gmail.com>
2023-01-09 10:37:51 +0300

    only datetime branch

--- app.py
+++ app.py
@@ -4,24 +4,6 @@ import datetime as dt
 from flask import Flask, jsonify
 
 
-from w1thermsensor import W1ThermSensor
-from libs.pressure import PressureSensor
-
-
-sensor = W1ThermSensor()
-
-def get_temperature():
-    return sensor.get_temperature() 
-
-
-def get_pressure():
-    return PressureSensor(0x77).get_pressure()
-
-
-def get_pressure_temp():
-    return PressureSensor(0x77).get_temp()
-
-
 app = Flask(__name__)
 
 
@@ -29,11 +11,7 @@ app = Flask(__name__)
 def index():
     return jsonify({
 	"dt": dt.datetime.now(),
-	"sensors": {
-		"t": [{"value": get_temperature(), "type": "ds18b20", "place": "out"},
-			{"value": get_pressure_temp(), "type": "bmp180", "place": "in"}],
-		"p": [{"value": get_pressure(), "type": "bmp180"}]
-	}
+	"sensors": []
 })
 
 
