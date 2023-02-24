0eac51f0a204c3d45889b5e6814c18e01718a762
urevoleg <urevolegg@gmail.com>
2023-02-24 10:33:50 +0300

    fix workers=2

--- sensor-app.sh
+++ sensor-app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /root/projects/orangepi-wstation && source venv/bin/activate
-gunicorn app:app -b 0.0.0.0:8080 --workers=4 --reload
+gunicorn app:app -b 0.0.0.0:8080 --workers=2 --reload
