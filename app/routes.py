0cae6f0f1f68e59359c24d85c38efc696720c253
urevoleg <urevolegg@gmail.com>
2023-04-02 17:40:22 +0300

    fix api sensors

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,10 @@ from flask import jsonify
 
 @app.route('/sensors')
 def sensors():
-    row = db.session.query(models.Sensor.category, models.Sensor.loaded_at, models.Sensor.json_data) \
-        .order_by(models.Sensor.loaded_at.desc()) \
-        .first()
-    return jsonify({**row})
+    categories = db.session.query(models.Sensor.category).distinct()
+    data = [{**db.session.query(models.Sensor.category, models.Sensor.loaded_at, models.Sensor.json_data) \
+        .order_by(models.Sensor.loaded_at.desc())\
+        .filter(models.Sensor.category==category)\
+        .first()} for category in categories]
+
+    return jsonify(data)
