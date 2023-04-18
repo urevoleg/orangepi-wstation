01e5ea12eb737c9560f0c5555b08431b3dfc8fcb
urevoleg <urevolegg@gmail.com>
2023-04-18 19:52:50 +0300

    fix get_forecast

--- app/forecast.py
+++ app/forecast.py
@@ -58,11 +58,12 @@ def formatted_forecast(prev_p, cur_p, name_of_useless_data='last_hour'):
 
 
 def get_forecast():
+    name_of_useless_data = 'last_hour'
     is_existing_prev_data = db.session.query(func.count(models.Sensor.id))\
         .order_by(models.Sensor.loaded_at.desc()) \
         .filter(models.Sensor.category == 'weather-out') \
         .filter(models.Sensor.loaded_at >= dt.datetime.now() - dt.timedelta(hours=1, minutes=5),
-                models.Sensor.loaded_at < dt.datetime.now() - dt.timedelta(hours=1)).scalar() or False
+                models.Sensor.loaded_at < dt.datetime.now() - dt.timedelta(hours=1)) or False
 
     if is_existing_prev_data:
         last_hour = db.session.query(models.Sensor.category, models.Sensor.loaded_at, models.Sensor.json_data) \
