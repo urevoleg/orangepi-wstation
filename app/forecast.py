9af2ab0c2dd4e6b1b17ca98fe68c26b6413c766f
urevoleg <urevolegg@gmail.com>
2023-04-18 13:35:20 +0300

    fix get_forecast

--- app/forecast.py
+++ app/forecast.py
@@ -54,6 +54,13 @@ def formatted_forecast(prev_p, cur_p):
 
 
 def get_forecast():
+
+    is_no_prev_data = db.session.query(True)\
+        .order_by(models.Sensor.loaded_at.desc()) \
+        .filter(models.Sensor.category == 'weather-out') \
+        .filter(models.Sensor.loaded_at >= dt.datetime.now() - dt.timedelta(hours=1, minutes=5),
+                models.Sensor.loaded_at < dt.datetime.now() - dt.timedelta(hours=1)).first()
+
     last_hour = db.session.query(models.Sensor.category, models.Sensor.loaded_at, models.Sensor.json_data) \
         .order_by(models.Sensor.loaded_at.desc()) \
         .filter(models.Sensor.category == 'weather-out') \
@@ -68,8 +75,7 @@ def get_forecast():
     try:
         return formatted_forecast(mean(row_handler(row) for row in last_hour), mean(row_handler(row) for row in current_hour))
     except StatisticsError as e:
-        app.logger.debug(last_hour == [])
-        app.logger.debug([*last_hour])
+        app.logger.debug(is_no_prev_data)
         #TODO baseline
         # Если каких-то данных нет, например, текущих или час назад, то надо что-то делать
         # например, можно взять среднее за последние 3\6\12 (по выбору) часов
