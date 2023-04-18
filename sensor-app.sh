c05ffd2bf15647360adf71cb1dd86d0413f798a1
urevoleg <urevolegg@gmail.com>
2023-04-18 12:55:12 +0300

    fix sensor-app.sh

--- sensor-app.sh
+++ sensor-app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /root/projects/orangepi-wstation && source venv/bin/activate
-gunicorn app:app -b 0.0.0.0:8080 --workers=2 --reload
+cd ~/projects/orangepi-wstation && source venv/bin/activate
+gunicorn app:app -b 0.0.0.0:5000 --workers=2 --reload
