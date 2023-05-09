36093385dc67abda70a480d6dd46efd9fb43ba54
urevoleg <urevolegg@gmail.com>
2023-05-09 07:51:13 +0300

    fix app debug=False

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 
 class Config(object):
-    DEBUG = True
+    DEBUG = False
     SECRET_KEY = os.getenv('SECRET_KEY') or 'you-will-never-guess'
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
