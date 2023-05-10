bffa6829a8d6cb921efea107d1e1bcc4f79da018
urevoleg <urevolegg@gmail.com>
2023-05-10 10:56:49 +0300

    fix gunicorn --preload

--- sensor-app.sh
+++ sensor-app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/userad/projects/orangepi-wstation && source venv/bin/activate
-gunicorn app:app -b 0.0.0.0:8880 --workers=2 --log-level INFO
+gunicorn app:app -b 0.0.0.0:8880 --workers=2 --log-level INFO --preload
