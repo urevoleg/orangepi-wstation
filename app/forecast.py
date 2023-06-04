010e189e31125c333b1b95d4bea4ebec3268d2bd
urevoleg <urevolegg@gmail.com>
2023-06-04 08:42:49 +0300

    fix is_existing_prev_data

--- app/forecast.py
+++ app/forecast.py
@@ -59,11 +59,11 @@ def formatted_forecast(prev_p, cur_p, name_of_useless_data='last_hour'):
 
 def get_forecast():
     name_of_useless_data = 'last_hour'
-    is_existing_prev_data = db.session.query(func.count(models.Sensor.id))\
+    is_existing_prev_data = db.session.query(models.Sensor.id)\
         .order_by(models.Sensor.loaded_at.desc()) \
         .filter(models.Sensor.category == 'weather-out') \
         .filter(models.Sensor.loaded_at >= dt.datetime.now() - dt.timedelta(hours=1, minutes=5),
-                models.Sensor.loaded_at < dt.datetime.now() - dt.timedelta(hours=1)).scalar() or False
+                models.Sensor.loaded_at < dt.datetime.now() - dt.timedelta(hours=1)).count() or False
 
     app.logger.debug(("is_existing_prev_data", is_existing_prev_data))
 
